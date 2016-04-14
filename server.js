fb5aab2626dc1360d53b042f180b17e966972850
LLama <billy.korbey.wright@gmail.com>
2016-04-14 21:23:44 +0000

    add url shortening; add redirect to pre-existing short urls; initialise database

--- server.js
+++ server.js
@@ -1,84 +1,61 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
+'use strict';
 
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
+const express = require('express');
+const validator = require('url-valid');
+const mongo = require('mongodb');
+const rs = require('random-strings');
+const mongoClient = mongo.MongoClient;
+const app = express();
+const url = 'mongodb://localhost:27017/db';
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
-
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
-
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
+app.get('/:query', (req, res) => {
+  mongoClient.connect(url, (err, db) => {
+    if (err) {
+      console.log(err);
+      res.writeHead(500);
+      res.end();
+      return;
     }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
+    const collection = db.collection('pages')
+    collection.find({ "short": `https://url-shortener-llamatarianism.c9users.io/${req.params.query}`})
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.writeHead(500);
+        res.end();
+        return;
+      }
+      if (data[0].hasOwnProperty('long')) {
+        res.redirect(data[0].long);
+      } else {
+        res.json({ "error": "page not found" });
+      }
+    });
   });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
+
+app.get(/\/new\/(http(s)?:\/\/){1}(www\.)?(.+)(\.com)+/i, (req, res) => {
+  if (req.params[1] === 's') req.params.splice(1, 1);
+  const requestUrl = req.params.join('');
+  
+  mongoClient.connect(url, (err, db) => {
+    if (err) {
+      console.log(err);
+      res.writeHead(500);
+      res.end();
+      return;
+    }
+    const collection = db.collection('pages');
+    const randString = rs.alphaNumMixed(16);
+    
+    const jsonResponse = {
+      "long": requestUrl,
+      "short": `https://url-shortener-llamatarianism.c9users.io/${randString}`
+    };
+    res.json(jsonResponse);
+    collection.insert(jsonResponse);
+    return;
+  });
+});
+
+app.listen(process.env.PORT);
